47b4a8e419586d60c607f942094ca1dd42364006
llanoxdewa <llanoxdew4@gmail.com>
2024-05-14 23:52:10 +0700

    fix some bug

--- middleware.ts
+++ middleware.ts
@@ -9,7 +9,6 @@ export default NextAuth(authConfig).auth
 export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
   matcher: [
-    '/dashboard/(.*)',
     '/((?!api|_next/static|_next/image|.*\\.png$).*)',
   ]
 };
